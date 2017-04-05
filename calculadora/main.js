1df50b4aeab31c3bc4f1a4633737d2db6ede773d
martileos <martinevarez@gmail.com>
2017-04-05 15:00:04 -0600

    Calculadora con electron -inicio

---
+++ calculadora/main.js
@@ -0,0 +1,35 @@
+// const app = require('electron').app
+// const BrowserWindow = require('electron').BrowserWindow
+const {app,BrowserWindow} = require('electron')
+//Ruta donde se encuentra nuestro proyecto
+const path = require('path')
+//Ruta pero en formato URL
+const url  = require('url')
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal = new BrowserWindow({
+		width:1024,
+		height:768
+	})
+	PantallaPrincipal.on('closed',function(){
+		PantallaPrincipal = null
+	})
+	PantallaPrincipal.loadURL('http://platzi.com')
+	PantallaPrincipal.show()
+}
+
+//La aplicación ejecuta este evento cuando
+//el archivo main.js se carga en memoria.
+app.on('ready',muestraPantallaPrincipal)
+
+
+
+
+
+
+
+
+
+
+
